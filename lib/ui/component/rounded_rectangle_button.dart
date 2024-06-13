c4f20591ca572fcc2651cb61a2942ed8bb3d0ba6
ErroredPasta <ForeignServices3132@pm.me>
2024-06-14 06:12:20 +0900

    feature: updated detail screen ui

---
+++ lib/ui/component/rounded_rectangle_button.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final roundedRectangleButtonStyle = FilledButton.styleFrom(shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))));
--- lib/ui/screen/todo_detail/todo_detail_screen.dart
+++ lib/ui/screen/todo_detail/todo_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/domain/model/todo.dart';
+import 'package:todo_app/ui/component/rounded_rectangle_button.dart';
 import 'package:todo_app/ui/screen/todo_edit/todo_edit_screen.dart';
 import 'package:todo_app/ui/util/date_time_formatter.dart';
 
@@ -11,35 +12,73 @@ class TodoDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            todo.title,
-            style:
-                Theme.of(context).textTheme.bodyLarge!.copyWith(fontSize: 20),
-          ),
-          const SizedBox(height: 32),
-          if (todo.dateTime != null)
-            Text('needs to be done until ${formatDateTime(todo.dateTime!)}'),
-          if (todo.dateTime != null) const SizedBox(height: 32),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Row(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (todo.dateTime != null) DetailScreenDateTimeSection(todo.dateTime!),
+            if (todo.dateTime != null) const SizedBox(height: 16),
+            const SizedBox(height: 16),
+            DetailScreenNoteSection(todo.note),
+            const SizedBox(height: 16),
+            const Spacer(),
+            Row(
               children: [
-                ElevatedButton(
-                  onPressed: () => _showEditScreen(context, todo),
-                  child: const Text('Edit'),
+                Expanded(
+                  child: FilledButton(
+                    style: roundedRectangleButtonStyle.copyWith(
+                      backgroundColor: WidgetStatePropertyAll(
+                        Theme.of(context).colorScheme.primaryContainer,
+                      ),
+                      foregroundColor: WidgetStatePropertyAll(
+                        Theme.of(context).colorScheme.onPrimaryContainer,
+                      ),
+                    ),
+                    onPressed: () => _showEditScreen(context, todo),
+                    child: const Text('Edit Todo'),
+                  ),
                 ),
-                ElevatedButton(
-                  child: const Text('Close'),
-                  onPressed: () =>
-                      Navigator.of(context, rootNavigator: true).pop(),
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: FilledButton(
+                    style: roundedRectangleButtonStyle.copyWith(
+                      backgroundColor: WidgetStatePropertyAll(
+                        Theme.of(context).colorScheme.errorContainer,
+                      ),
+                      foregroundColor: WidgetStatePropertyAll(
+                        Theme.of(context).colorScheme.onErrorContainer,
+                      ),
+                    ),
+                    child: const Text('Delete Todo'),
+                    onPressed: () {},
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(
+                style: roundedRectangleButtonStyle.copyWith(
+                  backgroundColor: WidgetStatePropertyAll(
+                    Theme.of(context).colorScheme.primaryContainer,
+                  ),
+                  foregroundColor: WidgetStatePropertyAll(
+                    Theme.of(context).colorScheme.onPrimaryContainer,
+                  ),
+                ),
+                onPressed: () {},
+                child: Text('Mark as ${todo.done ? "Not Done" : "Done"}'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -48,5 +87,53 @@ class TodoDetailScreen extends StatelessWidget {
     Navigator.of(context).push(MaterialPageRoute(
       builder: (ctx) => TodoEditScreen(todo),
     ));
+  }
+}
+
+class DetailScreenDateTimeSection extends StatelessWidget {
+  final DateTime dateTime;
+
+  const DetailScreenDateTimeSection(this.dateTime, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Due Date',
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(fontWeight: FontWeight.bold),
+        ),
+        const Spacer(),
+        Text(formatDateTime(dateTime))
+      ],
+    );
+  }
+}
+
+class DetailScreenNoteSection extends StatelessWidget {
+  final String? noteContent;
+
+  const DetailScreenNoteSection(this.noteContent, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Note',
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(noteContent != null ? noteContent! : 'No note')
+      ],
+    );
   }
 }
