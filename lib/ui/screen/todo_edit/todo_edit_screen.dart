e5e9e8d4828a052e0a7627495d01db53c9e41b89
ErroredPasta <ForeignServices3132@pm.me>
2024-06-15 02:24:33 +0900

    feature: updated edit screen ui

--- lib/ui/screen/todo_edit/todo_edit_screen.dart
+++ lib/ui/screen/todo_edit/todo_edit_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/domain/model/todo.dart';
+import 'package:todo_app/ui/component/rounded_rectangle_button.dart';
 import 'package:todo_app/ui/screen/todo_list_controller.dart';
 import 'package:todo_app/ui/util/date_time_formatter.dart';
+
+late AutoDisposeStateProvider<String> _todoTitle;
+late AutoDisposeStateProvider<String> _todoNote;
+late AutoDisposeStateProvider<DateTime?> _todoDateTime;
 
 class TodoEditScreen extends ConsumerStatefulWidget {
   final Todo todo;
@@ -10,101 +15,170 @@ class TodoEditScreen extends ConsumerStatefulWidget {
   const TodoEditScreen(this.todo, {super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() {
-    return _TodoEditState();
-  }
+  ConsumerState<TodoEditScreen> createState() => _TodoEditScreenState();
 }
 
-class _TodoEditState extends ConsumerState<TodoEditScreen> {
-  late AutoDisposeStateProvider<String> _todoInput;
-  late AutoDisposeStateProvider<DateTime?> _selectedDateProvider;
-
+class _TodoEditScreenState extends ConsumerState<TodoEditScreen> {
   @override
   void initState() {
     super.initState();
-    _todoInput = StateProvider.autoDispose<String>(
-      (ref) => widget.todo.title,
-    );
 
-    _selectedDateProvider = StateProvider.autoDispose<DateTime?>(
-      (ref) => widget.todo.dateTime,
-    );
+    final todo = widget.todo;
+
+    _todoTitle = StateProvider.autoDispose((ref) => todo.title);
+    _todoNote = StateProvider.autoDispose((ref) => todo.note ?? '');
+    _todoDateTime = StateProvider.autoDispose((ref) => todo.dateTime);
   }
 
   @override
   Widget build(BuildContext context) {
+    final todoTitle = ref.watch(_todoTitle);
+    final todoNote = ref.watch(_todoNote);
+    final todoDateTime = ref.watch(_todoDateTime);
     final todoController = ref.read(todoListControllerProvider.notifier);
-    final selectedDate = ref.watch(_selectedDateProvider);
-    final todoInput = ref.watch(_todoInput);
 
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(Icons.close),
+        ),
+        title: const Text('Edit Todo'),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextFormField(
-              initialValue: todoInput,
+              initialValue: todoTitle,
               decoration: const InputDecoration(
-                labelText: 'Todo',
+                hintText: 'Enter Title',
+                border: OutlineInputBorder(),
               ),
-              onChanged: (value) => ref.read(_todoInput.notifier).state = value,
+              onChanged: (value) => ref.read(_todoTitle.notifier).state = value,
             ),
-            const SizedBox(height: 32.0),
-            Row(
-              children: [
-                Text(
-                  selectedDate == null
-                      ? 'No Date Chosen'
-                      : formatDateTime(selectedDate),
-                ),
-                const Spacer(),
-                if (selectedDate != null)
-                  ElevatedButton(
-                    onPressed: () =>
-                        ref.read(_selectedDateProvider.notifier).state = null,
-                    child: const Text('Delete Date'),
-                  ),
-                const SizedBox(
-                  width: 16,
-                ),
-                ElevatedButton(
-                  onPressed: () => _pickDateTime(context, ref),
-                  child: const Text('Choose Date'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 32.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Cancel'),
-                ),
-                const SizedBox(
-                  width: 16,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _submitTodo(todoInput, selectedDate, todoController);
-                    Navigator.of(context).pop();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        Theme.of(context).colorScheme.primaryContainer,
-                    foregroundColor:
-                        Theme.of(context).colorScheme.onPrimaryContainer,
-                  ),
-                  child: const Text('Add Todo'),
-                )
-              ],
+            const SizedBox(height: 32),
+            const EditScreenNoteSection(),
+            const SizedBox(height: 32),
+            EditScreenDateTimeSection(),
+            const Spacer(),
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(
+                style: roundedRectangleButtonStyle,
+                onPressed: () {
+                  _editTodo(
+                    todoTitle,
+                    todoNote,
+                    todoDateTime,
+                    todoController,
+                  );
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Edit Todo'),
+              ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _editTodo(
+    String title,
+    String note,
+    DateTime? dateTime,
+    TodoListController controller,
+  ) {
+    controller.editTodo(
+      widget.todo.copyWith(
+        title: title,
+        note: note.isEmpty ? null : note,
+        dateTime: dateTime,
+      ),
+    );
+  }
+}
+
+class EditScreenNoteSection extends ConsumerWidget {
+  const EditScreenNoteSection({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todoNote = ref.watch(_todoNote);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Notes',
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        TextFormField(
+          initialValue: todoNote,
+          minLines: 3,
+          maxLines: 6,
+          decoration: const InputDecoration(
+            hintText: 'Add Note',
+            border: OutlineInputBorder(),
+          ),
+          onChanged: (value) => ref.read(_todoNote.notifier).state = value,
+        ),
+      ],
+    );
+  }
+}
+
+class EditScreenDateTimeSection extends ConsumerWidget {
+  final _controller = TextEditingController();
+
+  EditScreenDateTimeSection({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todoDateTime = ref.watch(_todoDateTime);
+
+    if (todoDateTime != null) _controller.text = formatDateTime(todoDateTime);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Due Date',
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        TextFormField(
+          readOnly: true,
+          controller: _controller,
+          decoration: InputDecoration(
+            hintText: 'Pick a Date',
+            border: const OutlineInputBorder(),
+            suffixIcon: todoDateTime == null
+                ? IconButton(
+              icon: const Icon(Icons.calendar_month_rounded),
+              onPressed: () => _pickDateTime(context, ref),
+            )
+                : IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () =>
+              ref.read(_todoDateTime.notifier).state = null,
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -125,20 +199,9 @@ class _TodoEditState extends ConsumerState<TodoEditScreen> {
 
     if (pickedDate == null || pickedTime == null) return;
 
-    ref.read(_selectedDateProvider.notifier).state = pickedDate.add(Duration(
+    ref.read(_todoDateTime.notifier).state = pickedDate.add(Duration(
       hours: pickedTime.hour,
       minutes: pickedTime.minute,
-    ));
-  }
-
-  void _submitTodo(
-    String todoInput,
-    DateTime? selectedDate,
-    TodoListController todoController,
-  ) {
-    todoController.editTodo(widget.todo.copyWith(
-      title: todoInput,
-      dateTime: selectedDate,
     ));
   }
 }
